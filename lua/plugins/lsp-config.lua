596a044a44dbc11a821cb720e07d00697eec2282
legi <iggypanolino@gmail.com>
2025-02-02 22:53:33 -0500

    fixed merge conflict

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,15 +9,26 @@ return {
 		"williamboman/mason.nvim",
 		lazy = false,
 		config = function()
-			require("mason").setup()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		lazy = false,
-		opts = {
-			auto_instal = true,
-		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "pyright" },
+				automatic_installation = true,
+			})
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
@@ -34,7 +45,7 @@ return {
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
 			})
-			lspconfig.html.setup({
+			lspconfig.pyright.setup({
 				capabilities = capabilities,
 			})
 			lspconfig.lua_ls.setup({
