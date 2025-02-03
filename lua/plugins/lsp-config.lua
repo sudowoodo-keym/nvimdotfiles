4140ed36b86a96db33f8df9b659633578d2198d9
sudowoodo-keym <iggypanolino@gmail.com>
2025-02-02 23:08:06 -0500

    added auto-install to mason-lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,10 +23,12 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = { "lua_ls", "ts_ls", "pyright" },
-				automatic_installation = true,
 			})
 		end,
 	},
