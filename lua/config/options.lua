7a535bc9050c92b3dbaaeeacc2c80fa1ffd3684c
keym <20496472+sudowoodo-keym@users.noreply.github.com>
2026-02-04 13:16:10 -0500

    changed options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,5 @@
 vim.opt.relativenumber = false
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
 vim.opt.expandtab = true
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
