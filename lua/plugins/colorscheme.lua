430642745d7c573c7a52b7913efc9b368302d791
keym <20496472+sudowoodo-keym@users.noreply.github.com>
2026-04-15 17:46:44 -0400

    commented out auto-switching logic

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,24 @@
 -- Auto-switching logic
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
-  callback = function()
-    local function set_colorscheme()
-      local hour = tonumber(os.date("%H"))
-
-      if hour >= 7 and hour < 18 then
-        vim.cmd.colorscheme("kanagawa-lotus")
-      else
-        vim.cmd.colorscheme("kanagawa-dragon")
-      end
-    end
-
-    set_colorscheme()
-
-    vim.fn.timer_start(600000, function()
-      set_colorscheme()
-    end, { ["repeat"] = -1 })
-  end,
-})
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = "VeryLazy",
+--   callback = function()
+--     local function set_colorscheme()
+--       local hour = tonumber(os.date("%H"))
+--
+--       if hour >= 7 and hour < 18 then
+--         vim.cmd.colorscheme("kanagawa-lotus")
+--       else
+--         vim.cmd.colorscheme("kanagawa-dragon")
+--       end
+--     end
+--
+--     set_colorscheme()
+--
+--     vim.fn.timer_start(600000, function()
+--       set_colorscheme()
+--     end, { ["repeat"] = -1 })
+--   end,
+-- })
 
 -- Plugin spec
 return {
