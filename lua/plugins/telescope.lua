32c74c71c6c4b903cf914e81985a4b89b8c1340d
sudowoodo-keym <20496472+sudowoodo-keym@users.noreply.github.com>
2026-04-17 14:54:41 -0400

    added obsidian-nvim and updated telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,9 @@
 -- plugins/telescope.lua:
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  -- or                              , branch = '0.1.x',
-  dependencies = { "nvim-lua/plenary.nvim" },
+    'nvim-telescope/telescope.nvim', version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    }
 }
